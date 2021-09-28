4991d804eec83d8ea0f6e82420df87156a2c5db1
Andrei Subbotin <subbkov@mail.ru>
2021-09-28 15:44:15 +0300

    feature/RGS-945/patient-statistics-endpoint
    Получить статистику пациентов по продукту мониторинга из РГС и датам

--- src/StatisticsRgsClient.php
+++ src/StatisticsRgsClient.php
@@ -5,6 +5,7 @@ namespace DocDoc\RgsApiClient;
 use DocDoc\RgsApiClient\Exception\BaseRgsException;
 use DocDoc\RgsApiClient\Exception\InternalErrorRgsException;
 use Psr\Http\Message\ResponseInterface;
+use DocDoc\RgsApiClient\Exception\BadRequestRgsException;
 
 class StatisticsRgsClient extends AbstractRgsClient
 {
@@ -24,4 +25,39 @@ class StatisticsRgsClient extends AbstractRgsClient
 
         return $this->send($request);
     }
+
+	/**
+	 * Получить статистику пациентов по продукту мониторинга из РГС и датам
+	 *
+	 * @param int $productId
+	 * @param string|null $period
+	 * @param string|null $fromDate
+	 * @param string|null $toDate
+	 *
+	 * @return ResponseInterface
+	 * @throws BaseRgsException
+	 * @throws BadRequestRgsException
+	 * @throws InternalErrorRgsException
+	 */
+	public function getPatientsStatisticsByProductAndDates(
+		int     $productId,
+		?string $period,
+		?string $fromDate,
+		?string $toDate
+	): ResponseInterface
+	{
+		$url = '/api/v1/statistics/' . $productId . '/patients';
+
+		$query = \array_filter([
+			'period' => $period,
+			'fromDate' => $fromDate,
+			'toDate' => $toDate,
+		]);
+
+		$url .= '?' . \http_build_query($query);
+
+		$request = $this->buildRequest('GET', $url, '');
+
+		return $this->send($request);
+	}
 }
